7100bfc51e621ceb4d34fae81908afe7a0c16e7c
Patrick <hello@patrick-kell.com>
2024-03-13 22:37:06 -0400

    Update Dodah.js

--- HN CREDIT LOG/Dodah.js
+++ HN CREDIT LOG/Dodah.js
@@ -1,39 +1,14 @@
-// function sendEmailOnThursday() {
-//   // Hardcoded recipient, subject, and content
-//   var recipient = "example@example.com";
-//   var emailSubject = "Your Subject Here";
-//   var emailContent = "Dear recipient,<br><br>This is the content of the email.<br><br>Sincerely,<br>Your Name";
-  
-//   // Get today's date
-//   var today = new Date();
-  
-//   // Check if today is Thursday and time is 4:30 AM
-//   if (today.getDay() == 4 && today.getHours() == 4 && today.getMinutes() == 30) {
-//     // Send the email
-//     MailApp.sendEmail({
-//       to: recipient,
-//       subject: emailSubject,
-//       htmlBody: emailContent
-//     });
-//   }
-// }
-
-function sendEmailOnSaturday() {
+function sendDodahsDateCheckEmail() {
   // Hardcoded recipient, subject, and content
   var recipient = "patrick.kell@momsorganicmarket.com";
-  var emailSubject = "Your Subject Here";
-  var emailContent = "Dear recipient,<br><br>This is the content of the email.<br><br>Sincerely,<br>Your Name";
-  
-  // Get today's date
-  var today = new Date();
-  
-  // Check if today is Saturday and time is within the 30-minute window
-  if (today.getDay() == 6 && today.getHours() == 14 && today.getMinutes() >= 30 && today.getMinutes() < 60) {
-    // Send the email
-    MailApp.sendEmail({
+  // var recipient = "hn.grocery@momsorganicmarket.com, hn.ops@momsorganicmarket.com, hn.customerservice@momsorganicmarket.com";
+  var emailSubject = "Dodah's Date Check";
+  var emailContent = "<p>Good morning team,</p><p>Please check the <strong><em>expiration dates of all Dodah's products</em></strong> on the shelf today. This helps us get credit for expired items when the delivery arrives this afternoon.</p><p>Let me know if you have any questions.</p><p>Thank you,</p>";
+
+  // Send the email
+  MailApp.sendEmail({
       to: recipient,
       subject: emailSubject,
       htmlBody: emailContent
-    });
-  }
+  });
 }
